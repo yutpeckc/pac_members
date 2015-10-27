a472e2327088a65d7846d5197219c71fe74b7d99
Qaid Damji <qdamji@gmail.com>
2015-10-26 19:13:49 -0700

    basic sorcery with reset_password -- untested

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  resources :password_resets
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
