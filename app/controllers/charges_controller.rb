57710b9561a34f0f9f055033b1e3a401abe29dc4
Qaid Damji <qdamji@gmail.com>
2015-11-03 21:17:35 -0800

    charges being made successfully with test keys

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,5 +4,37 @@ class ChargesController < ApplicationController
   end
 
   def create
+    # dump
+    # {"utf8"=>"✓", "authenticity_token"=>"sz63bqOpv2jwaDgSOgwS0EWEx9wPlTbuTF1hoOXr2k80MOHuw6hF9fgOEWm3qZb9DTQaShQMonJNTfd3dtVj2g==", "auto_bill"=>"true", "plan_id"=>"1", "plan_stripe_id"=>"pac_member_2015", "stripeToken"=>"tok_173J9zGacknxsRjuby4D3ter", "stripeTokenType"=>"card", "stripeEmail"=>"qdamji@gmail.com", "controller"=>"charges", "action"=>"create"}
+
+    plan = Plan.find(params[:plan_id])    
+
+    if params[:auto_bill] == "true"
+      customer = Stripe::Customer.create(
+        :email => current_user.email,
+        :card => params[:stripeToken],
+        :plan => plan.plan_stripe_id
+      )      
+      current_user.update_sub_info({plan: plan, expiration: 1.year.from_now, customer_id: customer.id, auto_bill: true})
+    else
+      customer = Stripe::Customer.create(
+        :email => current_user.email,
+        :card => params[:stripeToken]
+      )      
+      
+      charge = Stripe::Charge.create(
+        :customer => customer.id,
+        :amount => plan.price,
+        :description => plan.name,
+        :currency => 'cad'
+      )
+      current_user.update_sub_info({plan: plan, expiration: 1.year.from_now, customer_id: customer.id, auto_bill: false})      
+    end
+  
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to charges_path
+
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104035703) do
+ActiveRecord::Schema.define(version: 20151104045535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20151104035703) do
     t.string   "stripe_customer_id"
     t.datetime "membership_expiration"
     t.boolean  "auto_renew"
-    t.string   "subscription_id"
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "plan_id"
