b3cb11f3233de18c760de8452f421f96c64bddba
Qaid Damji <qdamji@gmail.com>
2015-11-04 21:58:00 -0800

    removed useless udpate_sub... fn from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,23 +9,4 @@ class User < ActiveRecord::Base
 
   belongs_to :plan
 
-  def update_sub_info(options = {})
-    expiration = options.fetch(:expiration, false)
-    auto_bill = options.fetch(:auto_bill, false)
-    plan = options.fetch(:plan, false)
-    customer_id = options.fetch(:customer_id, false)
-    if expiration
-      self.membership_expiration = expiration
-    end
-    if auto_bill
-      self.auto_renew = auto_bill
-    end
-    if plan
-      self.plan_id = plan
-    end    
-    if customer_id
-      self.stripe_customer_id = customer_id
-    end    
-    self.save
-  end
 end
