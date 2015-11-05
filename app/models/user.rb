a24550bfa623e85c0fdd4f82d9ddbdab552aa80e
Qaid Damji <qdamji@gmail.com>
2015-11-04 22:01:10 -0800

    added first and last name as requirements during user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
 	validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
 	validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
 	validates :email, uniqueness: true	
 
