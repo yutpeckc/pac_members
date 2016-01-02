e15d81b0413cd4224c0b47a42f860286ccdd4ef3
Qaid Damji <qdamji@gmail.com>
2016-01-01 20:55:14 -0500

    fixed error on pages plan

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,10 +2,11 @@ class ChargesController < ApplicationController
   def new
     if current_user.membership_expiration.present? && current_user.plan_id.present?
       @plans = []
-      @plans << current_user.plan_id
+      @plans << Plan.find(current_user.plan_id)
     else
       @plans = Plan.all.where(active: true)
     end
+
     @size = 4
     case @plans.count
     when 1
